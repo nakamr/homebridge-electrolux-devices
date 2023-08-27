494253c216e7fc937b15f844a470f612a937b4fe
Mathias Fredriksson <mafredri@gmail.com>
2023-08-27 21:55:53 +0300

    feat: improve appliance info
    This commit adds support for fetching information about appliances
    (Homebridge appliance cache must be deleted first) and showing the
    manufacturer based on brand (ELECTROLUX, AEG, etc.). It also parses the
    serial from the `applianceId` and shows it in HomeKit.
    
    For Air Purifiers, the firmware version is now also shown.

--- src/definitions/appliance.ts
+++ src/definitions/appliance.ts
@@ -39,6 +39,7 @@ type ApplianceProperties = {
     Temp: number;
     Humidity: number;
     TVOC: number;
+    FrmVer_NIU: string; // 3.0.1
 
     // Check last update time from metadata to figure out which.
     ECO2?: number;
@@ -68,3 +69,30 @@ type State = 'good';
 
 // Air purifiers.
 type WorkMode = 'Manual' | 'Auto' | 'PowerOff';
+
+/*
+Example payload:
+
+  {
+    "pnc": "950011538",
+    "brand": "ELECTROLUX",
+    "market": "EUROPE",
+    "productArea": "WELLBEING",
+    "deviceType": "AIR_PURIFIER",
+    "project": "HIMALAYA",
+    "model": "A9",
+    "variant": "CADR600",
+    "colour": "DARKGREY"
+  }
+*/
+export interface ApplianceInfo {
+    pnc: string;
+    brand: string;
+    market: string;
+    productArea: string;
+    deviceType: string;
+    project: string;
+    model: string;
+    variant: string;
+    colour: string;
+}
